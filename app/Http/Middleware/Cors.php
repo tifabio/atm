ef7545ef0276171893220a2985ee69160508fcba
Fabio Andrei Lorentz <tifabio@gmail.com>
2020-11-23 15:51:06 -0300

    Fix CORS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,6 +17,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', env('APP_SWAGGER'))
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Headers', '*')
+            ->header('Access-Control-Allow-Methods', '*');
     }
 }
